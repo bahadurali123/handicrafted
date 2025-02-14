111b2e677d97c8729efd96e67ba5894da3da8e15
bahadurali123 <bahadurali123@gmail.com>
2025-02-14 10:24:47 +0500

    Use sameSite: "None" for cross-site cookie sharing

--- controllers/user controllers/signin.controller.js
+++ controllers/user controllers/signin.controller.js
@@ -36,7 +36,7 @@ const SignIn = async (req, res) => {
             httpOnly: true,
             secure: true,
             // maxAge: 1000 * 60 * 60 * 24 * 30,
-            // sameSite: "None", // Allows cross-site cookies
+            sameSite: "None", // Allows cross-site cookies
             // domain: `${Configuration.CookieDomain}`, // Set the specific domain
         };
 
